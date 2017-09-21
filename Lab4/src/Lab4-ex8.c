dd7773df410662cfdd01d1af6b66b81f67bfc64f
Dalton Koch <dhktg2@mail.missouri.edu>
2017-09-21 11:25:02 -0500

    removed * from dyn_s = malloc and fixed printf statement

--- Lab4/src/Lab4-ex8.c
+++ Lab4/src/Lab4-ex8.c
@@ -8,10 +8,10 @@ int main(void)
 	printf("Enter the input string\n");
 	scanf("%s",s);
 	ln = strlen(s);
-	*dyn_s = (char*)malloc(strlen(s)+1);
+	dyn_s = (char*)malloc(strlen(s)+1);
 	dyn_s = s;
 	dyn_s[strlen(s)]='\0';
-	printf(dyn_s);
+	printf("%s",dyn_s);
 	return 0;
 }
 
