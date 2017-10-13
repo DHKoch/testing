1cfd6538f7140537267439aab09cb410d43f8a3c
Dalton Koch <dhktg2@mail.missouri.edu>
2017-10-13 11:51:46 -0500

    finished source codes of lab 5

--- Lab5/src/lab5.c
+++ Lab5/src/lab5.c
@@ -29,26 +29,36 @@ int main(int argc, char* argv[]){
 	int normalflag = 0;
 	
 	for(i=1;i<argc;i++){
-		
+		//loops through all the command line arguments and checks for errors
 		if((argv[i][0] == '-') && (argv[i][1] == 'n')){
+			//filenum argument
 			i++;
 			if(argv[i] == NULL){
 				error();
 				return 0;
 			}
+			//converts filenum string to int
 			fileNum = atoi(argv[i]);
 			if(fileNum == 0){
+				//checks for incorrect filenum
+				error();
+				return 0;
+			}
+			if(fileNum > 10){
+				//checks for incorrect filenum
 				error();
 				return 0;
 			}
 		}
 		
 		else if((argv[i][0] == '-') && (argv[i][1] == 'o')){
+			//offset vlaue argument 
 			i++;
 			if(argv[i] == NULL){
 				error();
 				return 0;
 			}
+			//converts string to double
 			offset = strtod(argv[i],argv);
 			if(offset == 0){
 				error();
@@ -57,68 +67,85 @@ int main(int argc, char* argv[]){
 		}
 		
 		else if((argv[i][0] == '-') && (argv[i][1] == 'S')){
-			i++;
+			//do stats flag is set
+
 			statsflag = 1;
 		}
 		
 		else if((argv[i][0] == '-') && (argv[i][1] == 'C')){
-			i++;
+			printf("here 4\n");
+			//do center flag is set
+			
 			centerflag = 1;
 		}
 		
 		else if((argv[i][0] == '-') && (argv[i][1] == 'N')){
-			i++;
+			//do normal flag is set
+			printf("here 3\n");
+
 			normalflag = 1;
 		}
 		
 		else if((argv[i][0] == '-') && (argv[i][1] == 's')){
+			//scale argument
 			i++;
 			if(argv[i] == NULL){
+				//checks for nothing after -s
 				error();
 				return 0;
 			}
+			//converts string to double
 			scale = strtod(argv[i],argv);
 			if(scale == 0){
+				//error checking
 				error();
 				return 0;
 			}
 		}
 		
 		else if((argv[i][0] == '-') && (argv[i][1] == 'r')){
+			// sets new name for output file
 			i++;
 			if(argv[i] == NULL){
+				//checks for incorretc argument
 				error();
 				return 0;
 			}
+			//copies new file name into variable and sets a flag
 			strcpy(outFile2,argv[i]);
 			flag1 = 1;
 		}
 		
 		else if((argv[i][0] == '-') && (argv[i][1] == 'h')){
+			//prints out how to use the program
 			printf("Ther correct use of this program is as follows:\n");
-			printf("./programName -h\nOr\n./progName -n 'file number' -o 'int' -s 'double' -r 'Output file name'\n");
+			printf("./programName -h\nOr\n./progName -n 'file number' -o 'int' -s 'double' -r 'Output file name' -S -N -C\n");
 			return 0;
 		}
 		
 		else{
+			//handles any other case than those stated above
 			error();
 			return 0;
 		}
 		
 	}
-	printf("%d %f %f %s \n",fileNum,offset,scale,outFile);
-	array = readfile(fileNum);
-	for(i=0;i<6;i++){
+	//printf("%d %f %f %s \n",fileNum,offset,scale,outFile);
+	array = readfile(fileNum); //read in signal from input file
+	/*for(i=0;i<6;i++){
 		printf("%d\n",*(array+i));
-	}
+	}*/
 	
 	if(flag1 == 1){
+		//copies the original input into an output file with the
+		//specified name for -r argument 
 		copy(array,outFile2);
 	}
 	
 	if(offset != 0){
+		// sets output file name to offset
 		sprintf(outFile,"Offset_data_%02d.txt",fileNum);
-		do_offset(array,offset,outFile);
+		do_offset(array,offset,outFile); 
 	}
 	
 	if(scale != 0){
@@ -127,73 +154,83 @@ int main(int argc, char* argv[]){
 	}
 	
 	if(statsflag == 1){
+		//does stats operations
 		sprintf(outFile,"Stats_data_%02d.txt",fileNum);
-		double average = find_avg(array);
-		int max = find_max(array);
+		double average = find_avg(array); //average is needed for the save stats file
+		int max = find_max(array); //max is needed for the save stats file
 		save_stats(average,max,outFile);
 	}
-	
+	printf("here1\n");
 	if(normalflag == 1){
+		printf("here\n");
+		//does normal operations
 		sprintf(outFile,"Normal_data_%02d.txt",fileNum);
-		int maxval = find_maxval(fileNum);
+		int maxval = find_maxval(fileNum); //max val is needed to normalize
 		printf("Maxval: %d\n",maxval);
 		save_normal(array,maxval,outFile);
 	}
 	
 	if(centerflag == 1){
 		sprintf(outFile,"Center_data_%02d.txt",fileNum);
-		double average1 = find_avg(array);
+		double average1 = find_avg(array); // average is needed to center the data
 		save_center(average1,array,outFile);
 	}
-
+	//free up memory
 	free(array);
 	free(outFile);
+	free(outFile2);
 	return 0;
 }
 
 void error(void){
+	//prints out error and how to use the program 
 	printf("Incorrect use of program!\nThe correct use is as follows:\n");
-	printf("./programName -h\nOr\n./progName -n 'file number' -o 'int' -s 'double' -r 'Output file name'\n");
+	printf("./programName -h\nOr\n./progName -n 'file number' -o 'int' -s 'double' -r 'Output file name' -S -N -C\n");
 	
 }
 
+//reads data from file
 int* readfile(int fileNum){
 	int i = 1;
 	char* filename = malloc(sizeof(char)*16);
-	sprintf(filename,"Raw_data_%02d.txt",fileNum);
-	printf("%s\n",filename);
+	sprintf(filename,"Raw_data_%02d.txt",fileNum); //sets correct file name based on filenum
+	//printf("%s\n",filename);
 	FILE* fp = fopen(filename,"r");
 	if(fp == NULL){
+		//checks to see if file could be opened
 		printf("file not opened\n");
 	}
 	
 	int length = 0;
 	int maxval = 0;
+	//reads in the length of the fiel and max value
 	fscanf(fp,"%d %d",&length,&maxval);
-	
+	//allocates memory for the array
 	int* array = malloc(sizeof(int)*(length+1));
+	//length is set as the first value in the array
 	*array = length;
 	for(i=1;i<=length;i++){
+		//loops through and sets all other values in the array
 		fscanf(fp,"%d",(array+i));
-		printf("%d\n",*(array+i));
+		//printf("%d\n",*(array+i));
 	}
-	fclose(fp);
+	fclose(fp); // close file
 	return array;
 }
 
 void do_offset(int* array, double offset, char* outFile){
 	int length = *array;
 	FILE* fp = fopen(outFile,"w");
-	if(fp == NULL){
+	if(fp == NULL){ //checks to see if file could be opened
 		printf("file not opened\n");
 	}
 	int i = 1;
-	fprintf(fp,"%d %.4f\n",length,offset);
-	for(i=1;i<=length;i++){
+	fprintf(fp,"%d %.4f\n",length,offset); // first line in the file will have the length and offset value
+	for(i=1;i<=length;i++){ //prints the value+offset to the file
 		fprintf(fp,"%.4f\n",(double)(*(array+i) + offset));
 		
 	}
-	fclose(fp);
+	fclose(fp); //closes file
 }
 
 void do_scale(int* array, double scale,char* outFile){
@@ -203,8 +240,8 @@ void do_scale(int* array, double scale,char* outFile){
 		printf("file not opened\n");
 	}
 	int i = 1;
-	fprintf(fp,"%d %.4f\n",length,scale);
-	for(i=1;i<=length;i++){
+	fprintf(fp,"%d %.4f\n",length,scale); //first line in fiel will have length and scale
+	for(i=1;i<=length;i++){ //prints the value*scale to the file
 		fprintf(fp,"%.4f\n",(double)(*(array+i) * scale));
 		
 	}
@@ -216,10 +253,11 @@ void copy(int* array, char* outFile){
 	sprintf(filename,"%s.txt",outFile);
 	FILE* fp = fopen(filename,"w");
 	int i = 1;
-	for(i=1; i<=*array;i++){
+	for(i=1; i<=*array;i++){ //copies the input file data to the ouputfile
 		fprintf(fp,"%d\n",*(array+i));
 	}
 	fclose(fp);
+	free(filename);
 }
 
 double find_avg(int* array){
@@ -227,9 +265,10 @@ double find_avg(int* array){
 	int i = 1;
 	double total = 0;
 	double average = 0;
-	for(i=1;i<=length;i++){
+	for(i=1;i<=length;i++){ //totals all values in array
 		total = total + *(array + i);
 	}
+	//divide total by length for average
 	average = total/(double)length;
 	return average;
 }
@@ -238,6 +277,7 @@ int find_max(int* array){
 	int length = *array;
 	int i = 1;
 	int max = *(array+1);
+	//loops and checks each value against the previous
 	for(i=1;i<=length;i++){
 		if(*(array+i)>max){
 			max = *(array+i);
@@ -248,7 +288,7 @@ int find_max(int* array){
 
 void save_stats(double average, int max, char* filename){
 	FILE* fp = fopen(filename,"w");
-	fprintf(fp,"%.4f %d",average,max);
+	fprintf(fp,"%.4f %d",average,max); //prints the average and max to the stats file
 	fclose(fp);
 }
 
@@ -256,13 +296,14 @@ void save_center(double average, int* array, char* outFile){
 	FILE* fp = fopen(outFile,"w");
 	int length = *array;
 	int i =1;
-	for(i=1;i<=length;i++){
-		fprintf(fp,"%.4f\n",(double)*(array+i) - average);
+	for(i=1;i<=length;i++){ //center data so new average will be 0
+		fprintf(fp,"%.4f\n",(double)*(array+i) - average); //prints new value to center file
 	}
 	fclose(fp);
 }
 
-int find_maxval(int filenum){
+int find_maxval(int filenum){ 
+	// read the first line of the file to get the max value
 	char* filename = malloc(sizeof(char)*16);
 	sprintf(filename,"Raw_data_%02d.txt",filenum);
 	FILE* fp = fopen(filename,"r");
@@ -273,14 +314,17 @@ int find_maxval(int filenum){
 	int length = 0;
 	int maxval = 0;
 	fscanf(fp,"%d %d",&length,&maxval);
+	//returns max value
 	return maxval;
 }
 
 void save_normal(int* array,int maxval,char* outFile){
+	//every value will be between -1 and 1
 	FILE* fp = fopen(outFile,"w");
 	int length = *array;
 	int i = 1;
 	for(i=1;i<=length;i++){
+		//prints new values into normal file
 		fprintf(fp,"%.4f\n",(double)*(array+i)/(double)maxval);
 	}
 	fclose(fp);
--- Lab5/src/lab5_part2.cpp
+++ Lab5/src/lab5_part2.cpp
@@ -4,8 +4,6 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
-#include <sstream>
-//#include <stdio.h>
 #include <cstdlib>
 
 using namespace std;
@@ -32,48 +30,58 @@ class Signal{
 		void sig_info();
 };
 
+// non - parametric constructor 
 Signal::Signal(){
-	
+	//ifstream so we can read from file
 	std::ifstream myfile;
 	myfile.open("Raw_data_01.txt");
 	if(!myfile.is_open()){
+		//checkss to see if file was opened
 		cout << "error file not opened" << endl;
 	}
 	
-	myfile >> length;
-	myfile >> max_val;
-	raw_data = new double[length];
+	myfile >> length; // reads in the length
+	myfile >> max_val; // reads in the max value
+	raw_data = new double[length]; // creates a new array of doubles
 	for(int i = 0; i<length;i++){
+		//reads in each value into the array
 		myfile >> raw_data[i];
 	}
-	myfile.close();
-	average = getaverage();
+	myfile.close(); // close file
+	average = getaverage(); // set average member
 }
-
+//parametric constructor 
 Signal::Signal(int filenum){
 	std::ifstream myfile;
 	char filename[30];
+	// uses file num to open a select file
 	int n = sprintf(filename,"Raw_data_%02d.txt",filenum);
 	myfile.open(filename);
 	if(!myfile.is_open()){
+		//checks to see if file was opened
 		cout << "error file not opened" << endl;
 	}
+	//reads in all the values
 	myfile >> length;
 	myfile >> max_val;
 	raw_data = new double[length];
 	for(int i = 0; i<length;i++){
 		myfile >> raw_data[i];
 	}
-	myfile.close();
-	average = getaverage();
+	myfile.close(); // closes file
+	average = getaverage(); // sets average member
 }
 
+//prarmetric constructor
 Signal::Signal(char* filename){
 	std::ifstream myfile;
+	//opens the file with the name set by the user
 	myfile.open(filename);
 	if(!myfile.is_open()){
+		//checks to see if file can be opened
 		cout << "error file not opened" << endl;
 	}
+	//reads in vlaues
 	myfile >> length;
 	myfile >> max_val;
 	raw_data = new double[length];
@@ -84,76 +92,91 @@ Signal::Signal(char* filename){
 	average = getaverage();
 }
 
+//destructor
 Signal::~Signal(){
 	delete raw_data;
 }
 
+//find the average
 double Signal::getaverage(){
 	double total = 0.0;
 	for(int i = 0;i<=length;i++){
-		total += raw_data[i];
+		total += raw_data[i]; // totals all values in signal
 	}
 	return(total/(double)length);
 }
 
 void Signal::do_offset(double offset){
 	for(int i=0; i<length; i++){
+		//adds the offset to each value in signal
 		raw_data[i] = raw_data[i] + offset;
 	}
+	//sets new average and max_val
 	average = getaverage();
 	max_val = getmax_val();
 }
 
 void Signal::do_scale(double scale){
 	for(int i=0; i<length; i++){
+		//multiplies each value in signal by scale
 		raw_data[i] = raw_data[i] * scale;
 	}
+	//sets new average and max_val
 	average = getaverage();
 	max_val = getmax_val();
 }
 
 void Signal::do_center(){
 	for(int i=0; i<length; i++){
+		//makes it to where the new average is 0
 		raw_data[i] = raw_data[i] - average;
 	}
+	//sets new average and max_val
 	average = getaverage();
 	max_val = getmax_val();
 }
 
 void Signal::do_normal(){
 	for(int i=0; i<length; i++){
+		//makes it to where each values is less than 1 or greater than -1
 		raw_data[i] = raw_data[i] / max_val;
 	}
+	//sets new average and max_val
 	average = getaverage();
 	max_val = getmax_val();
 }
 void Signal::do_stats(){
+	//gets the current average and max_value
 	average = getaverage();
 	max_val = getmax_val();
+	//displays the values
 	cout << "Average: " << average << "maximum: " << max_val << endl;
 }
 
 void Signal::print_to_file(char* filename){
-	std::ofstream myfile;
-	
-	myfile.open(filename);
+	std::ofstream myfile; //  ofstrema so we can write to file	
+	myfile.open(filename); // opens file
 	if(!myfile.is_open()){
+		//checks to see if file was opened 
 		cout << "error file not opened" << endl;
 	}
-	myfile << length;
-	myfile << ' ';
-	myfile << max_val;
-	myfile << '\n' << endl;
+	myfile << length; // writes the length
+	myfile << ' '; // writes a space char
+	myfile << max_val; // writes the max value
+	myfile << char(13) << char(10); // writes a new line
 	for(int i = 0; i<length;i++){
-		myfile << raw_data[i] << '\n' << endl;
+		// writes the value and then a new line
+		myfile << raw_data[i] << char(13) << char(10);
 		
 	}
 	myfile.close();
 }
 
+// gets the max value in the signal
 double Signal::getmax_val(){
 	double max = 0.0;
-	for(int i=0; i<length; i++){
+	//loop and compares each value to the previous lowest value
+	for(int i=0; i<length; i++){ 
 		if(raw_data[i]>max){
 			max = raw_data[i];
 		}
@@ -161,7 +184,8 @@ double Signal::getmax_val(){
 	return (max);
 }
 
-void Signal::sig_info(){
+// Prints the signal info
+void Signal::sig_info(){ 
 	cout << "Length: " << length << "Maximum: " << max_val << "Average: " << average << endl; 
 }
 
@@ -169,20 +193,20 @@ int main(int argc, char* argv[]){
 	int filenum = 0;
 	char filename[30];
 	int i = 1;
-	Signal* sig1;
-	if(argc == 1){
+	Signal* sig1; // pointer of Signal type used for the program
+	if(argc == 1){ // case for no command line arguments
 		Signal sig;
 		sig1 = &sig;
 	}
 	else{
 		if((argv[i][0] == '-') && (argv[i][1] == 'n')){
 			i++;
-			if(argv[i] == NULL){
+			if(argv[i] == NULL){ // makes sure a file num is after the -n 
 				cout << "error!" << endl;
 				return 0;
 			}
 			filenum = std::atoi(argv[i]);
-			if(filenum <= 0){
+			if(filenum <= 0){ // checks to make sure filenum is not 0 or less than 0
 				cout << "error, file number cannot be negative" << endl;
 				return 0;
 			}
@@ -191,15 +215,15 @@ int main(int argc, char* argv[]){
 		}
 		else if((argv[i][0] == '-') && (argv[i][1] == 'f')){
 			i++;
-			if(argv[i] == NULL){
+			if(argv[i] == NULL){ // checks to make sure there is a file name 
 				cout << "error!" << endl;
 				return 0;
 			}
-			stpcpy(filename,argv[i]);
-			Signal sig(filename);
-			sig1 = &sig;
+			stpcpy(filename,argv[i]); // copies input file name into filename variable
+			Signal sig(filename); // calls parametric constructor that handles filename
+			sig1 = &sig; // sets the pointer to the adress of the signal created 
 		}	
-		else{
+		else{ // case that handles wrong command line arguments
 			cout << "error, incorrect command line inputs" << endl;
 			return 0;
 		}
@@ -209,15 +233,16 @@ int main(int argc, char* argv[]){
 	double offset = 0.0;
 	double scale = 0.0;
 	char output[30];
-	while(j == 1){
+	while(j == 1){ // loop that reprints menu and allows new selection
 		cout << "Please select a action to preform:" << endl
 		<< "1.) Offset:" << endl
 		<< "2.) Scale:" << endl
 		<< "3.) Statistics:" << endl
 		<< "4.) Center:" << endl
 		<< "5.) Normalize:" << endl
-		<< "6.) Print Info:" << endl
-		<< "7.) Exit..." << endl;
+		<< "6.) Print To File:" << endl
+		<< "7.) Signal Info:" << endl
+		<< "8.) Exit..." << endl;
 		cin >> choice;
 		
 		switch(choice){
@@ -231,7 +256,7 @@ int main(int argc, char* argv[]){
 			case 2:
 			scale = 0.0;
 			cout << "Please enter a value to scale the signal by:";
-			cin >> offset;
+			cin >> scale;
 			sig1->do_scale(scale);
 			break;
 			
@@ -253,11 +278,20 @@ int main(int argc, char* argv[]){
 			case 6:
 			cout << "Please enter the name of the output file:" << endl;
 			cin >> output;
+			if(output == NULL){ // error check for file name
+				cout << "error name cannot be null, please try again" << endl;
+				break;
+			}
 			sig1->print_to_file(output);
 			break;
 			
 			case 7:
-			cout << "Exiting the program" << endl;
+			cout << "Printing the Signal Info ..." << endl;
+			sig1->sig_info();
+			break;
+			
+			case 8:
+			cout << "Exiting the program ..." << endl;
 			return 0;
 			break;
 			
@@ -268,7 +302,3 @@ int main(int argc, char* argv[]){
 	}
 	return 0;
 }
-
-
-
-
